b3e796d6316a89fd12c911c3011128290732913e
AlpRuzgar <alpruzgar@hotmail.com>
2026-08-31 21:41:53 +0300

    Initial Commit

---
+++ RunTracker/RunTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunTrackerApp.swift
+//  RunTracker
+//
+//  Created by Alp Rüzgar on 31.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct RunTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RunTrackerTests/RunTrackerTests.swift
@@ -0,0 +1,18 @@
+//
+//  RunTrackerTests.swift
+//  RunTrackerTests
+//
+//  Created by Alp Rüzgar on 31.08.2026.
+//
+
+import Testing
+
+struct RunTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
